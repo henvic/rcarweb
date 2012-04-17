3de0e337d1226b3c0b0c325482d78416d1b8de59
Henrique Vicente <henriquevicente@gmail.com>
2012-04-17 02:39:14 -0300

    add less support
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
