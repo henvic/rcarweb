2b08f14ccd673556db572007c2c46933d122c781
Henrique Vicente <henriquevicente@gmail.com>
2012-04-25 01:37:47 -0300

    add app basic set up
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- app.js
+++ app.js
@@ -11,26 +11,72 @@ var sio = require('socket.io'),
     config = require('./config');
 
 
-var app = express();
+/**
+ * create app
+ */
+var app = express.createServer(),
+    io = sio.listen(app),
+    parseCookie = connect.utils.parseCookie,
+    MemoryStore = connect.middleware.session.MemoryStore,
+    store;
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {layout: false});
+    app.use(express.cookieParser());
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(require('less-middleware')({ src: __dirname + '/public' }));
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.session({
+        secret: 'secret',
+        key: 'express.sid',
+        store: store = new MemoryStore()
+    }));
+    app.use(app.router);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+
+io.configure('development', function () {
+    io.set('log level', 3);
 });
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(3000);
+io.set('authorization', function (data, accept) {
+    if (!data.headers.cookie) {
+        return accept('No cookie transmitted.', false);
+    }
 
-console.log("Express server listening on port 3000");
+    data.cookie = parseCookie(data.headers.cookie);
+    data.sessionID = data.cookie['express.sid'];
+
+    store.load(data.sessionID, function (err, session) {
+        if (err || !session) {
+            return accept('Error', false);
+        }
+
+        data.session = session;
+        return accept(null, true);
+    });
+}).sockets.on('connection', function (socket) {
+    var sess = socket.handshake.session;
+    socket.log.info(
+        'a socket with sessionID',
+        socket.handshake.sessionID,
+        'connected'
+    );
+    socket.on('set value', function (val) {
+        sess.reload(function () {
+            sess.value = val;
+            sess.touch().save();
+        });
+    });
+});
+
+
+app.listen(config.port);
