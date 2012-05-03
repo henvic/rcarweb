6b830c86f30d3cd4efc227a9d072f3b9cb4b203a
Henrique Vicente <henriquevicente@gmail.com>
2012-05-03 01:33:42 -0300

    add grunt
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ grunt.js
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    test: {
+      files: ['test/**/*.js']
+    },
+    lint: {
+      files: ['grunt.js', 'app/**/*.js', 'config/**/*.js', 'db/**/*.js', 'public/javascripts/**/*.js', 'routes/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        globalstrict: true,
+        es5: true
+      },
+      globals: {
+        exports: true
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+
+};
