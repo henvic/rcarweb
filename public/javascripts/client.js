ed3fb8d4396a7126835ad7b71dbdc903479a0216
Henrique Vicente <henriquevicente@gmail.com>
2012-05-07 01:14:06 -0300

    change very simple JS console to a morse code message system
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -16,17 +16,88 @@
             }, timeout);
         });
 
-        $('#console-submit').click(function (e) {
-            var consoleTextarea = $('#console-textarea');
-            var consoleReturn = $('#console-return');
-            var returnHistory = [];
-            var cmd = consoleTextarea.val();
+        function morse(message, html) {
+            var table = [];
+            table["a"]=". _";
+            table["b"]="_ . . .";
+            table["c"]="_ . _ .";
+            table["d"]="_ . .";
+            table["e"]=".";
+            table["é"]=". . _ . .";
+            table["f"]=". . _ .";
+            table["g"]="_ _ .";
+            table["h"]=". . . .";
+            table["i"]=". .";
+            table["j"]=". _ _ _";
+            table["k"]="_ . _";
+            table["l"]=". _ . .";
+            table["m"]="_ _";
+            table["n"]="_ .";
+            table["o"]="_ _ _";
+            table["p"]=". _ _ .";
+            table["q"]="_ _ . _";
+            table["r"]=". _ .";
+            table["s"]=". . .";
+            table["t"]="_";
+            table["u"]=". . _";
+            table["v"]=". . . _";
+            table["w"]=". _ _";
+            table["x"]="_ . . _";
+            table["y"]="_ . _ _";
+            table["z"]="_ _ . .";
+            table["1"]=". _ _ _ _";
+            table["2"]=". . _ _ _";
+            table["3"]=". . . _ _";
+            table["4"]=". . . . _";
+            table["5"]=". . . . .";
+            table["6"]="_ . . . .";
+            table["7"]="_ _ . . .";
+            table["8"]="_ _ _ . .";
+            table["9"]="_ _ _ _ .";
+            table["0"]="_ _ _ _ _";
+            table["."]=". − . − . −";
+            table[","]="− − . . − −";
+            table[":"]="− − − . . .";
+            table["?"]=". . − − . .";
+            table["’"]=". − − − − .";
+            table["–"]="− . . . . −";
+            table["/"]="− . . − .";
+            table["("]="− . − − .";
+            table["("]="− . − − .";
+            table["“"]=". - . . - .";
+            table["”"]=". - . . - .";
+            table["="]="- . . . -";
+            table["+"]=". - . - .";
+            table["×"]="- . . -";
+            table["@"]=". - - . - .";
+            var code = '';
+            var chars = message.toLowerCase().split("");
+
+            for (var x = 0; x < chars.length; x++) {
+                if (chars[x] === " ") {
+                    code += "       ";
+                } else {
+                    if (table[chars[x]]) {
+                        if (html) {
+                            code += table[chars[x]].replace(/ /gi, "&nbsp;") + "&nbsp;&nbsp;&nbsp;";
+                        } else {
+                            code += table[chars[x]] + "   ";
+                        }
+                    }
+                }
+            }
+            
+            return code;
+        }
+
+        $('#morse-submit').click(function (e) {
+            var morseTextarea = $('#morse-textarea');
+            var morseReturn = $('#morse-return');
+            var message = morseTextarea.val();
 
             e.preventDefault();
 
-            var returnValue = eval(cmd);
-
-            consoleReturn.html('<i class="icon-arrow-right"></i> ' + returnValue);
+            morseReturn.html('<i class="icon-arrow-right"></i> ' + morse(message, true));
         });
 
         $('[data-spy="scroll"]').each(function () {
